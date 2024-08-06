5e413184e1bf7883ddae3dff26818fd61d79581c
Thanapong Khongkasem <137611532+skzx1@users.noreply.github.com>
2024-08-06 18:24:32 +0700

    init

--- lab5.py
+++ lab5.py
@@ -1,2 +1,12 @@
-print("Hello, world!")
-xm = 10
+height = int(input("Enter height: "))
+s = 3
+i = 1
+
+while height > 0:
+    if i == 1:
+        print(" " * (2 * (height - 1)) + "1")
+    else:
+        print(" " * (2 * (height - 1)) + "1" + " " * s + "1")
+        s += 4
+    height -= 1    
+    i += 1
