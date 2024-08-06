45071910861e62d80489b013bf5a44c2f8d57cf4
Thanapong Khongkasem <137611532+skzx1@users.noreply.github.com>
2024-08-06 19:05:05 +0700

    init

--- lab.py
+++ lab.py
@@ -1,26 +1,25 @@
 hours = int(input('Enter number of hours: '))
 minutes = int(input('Enter number of minutes: '))
 buyAmt = int(input('Enter shopping amount: '))
-tcost = 0
-if hours < 0 or hours >= 20 or minutes < 0 or minutes >= 60:
-    print("Invalid time.")
+if hours < 0 or hours > 20 or minutes < 0 or minutes > 59  or (hours == 20 and minutes > 0):
+    print(“Invalid time.”)
 else:
-        if minutes > 0:
-            thours = hours + 1
+    if minutes > 0:
+        hours += 1
+    if buyAmt >= 3001:
+        print(“No charge, thank you.”)
+    elif buyAmt >= 300:
+        if hours <= 4:
+            print(“No charge, thank you.”)
         else:
-            thours = hours
-        if buyAmt >= 3001 or thours <= 2:
-             print("No charge, thanks.")
+            charge = (hours - 4) * 50
+            print(f”Total amount due is {charge} Baht, thank you.”)
+    else:
+        if hours <= 2:
+            print(“No charge, thank you.”)
         else:
-            if buyAmt >= 300:
-                free = 4
+            if hours <= 4:
+                charge = (hours - 2) * 20
             else:
-                free = 2
-            if thours <= free:
-                tcost = 0
-            elif thours <= 4:
-                tcost = (thours - free) * 20
-            else:
-                tcost = (4 - free) * 20 + (thours - 4) * 50
-        
-            print(f"Total amount due is {tcost} Bahts, thank you.")
+                charge = 2 * 20 + (hours - 4) * 50
+            print(f”Total amount due is {charge} Baht, thank you.”)
