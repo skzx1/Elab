40a35ac040259c31e67a60c7658174aa5839ef9a
Frong <thanapong.kho@ku.th>
2025-02-08 13:56:16 +0700

    m

--- lab3.py
+++
@@ -1,8 +0,0 @@
-x = int(input())
-while x > 10:
-    sum = 0
-    while x > 0:
-       sum += x % 10
-       x //= 10
-    x = sum
-print(x)
--- lab4.py
+++
@@ -1,4 +0,0 @@
-g = int(input("Enter the first number: "))
-l = int(input("Enter the second number: "))
-sum = g % l
-print(sum)
